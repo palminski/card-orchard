9882882f8ebd2c37578332c33f53b4fd46448ac5
palminski <williambolls98@gmail.com>
2023-04-20 12:33:57 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -8,28 +8,36 @@ import Card from './pages/Card';
 import AllCards from './pages/AllCards';
 import NotFound from './pages/NotFound';
 
-import {Routes, Route, Link} from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    
-      <div className="App">
-        <Nav />
 
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route path="/all-cards" element={<AllCards />}></Route>
-          <Route path="/card" >
-            <Route path='' element={<Card />}></Route>
-            <Route path=':id' element={<Card />}></Route>
-          </Route>
-          <Route path="*" element={<NotFound/>}></Route>
-        </Routes>
+    <div className="App">
 
-        
+      <div id='flexBody'>
+        <div id='mainBody'>
+          <Nav />
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/all-cards" element={<AllCards />}></Route>
+            <Route path="/card" >
+              <Route path='' element={<Card />}></Route>
+              <Route path=':id' element={<Card />}></Route>
+            </Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </div>
 
+
+        <div id='footer' className='bg-dark'>  </div>
       </div>
-    
+
+
+    </div>
+
+
+
 
   );
 }
